8e8e19ec9d81d4fac6f043ac7952ff52f6312c44
saran <saranyapong272547@gmail.com>
2025-08-11 22:34:15 +0700

    bytes_biginteger

--- ctf-learning-template/ctf-crypto/CRYPTOHACK/Hex/hex.py
+++ ctf-learning-template/ctf-crypto/CRYPTOHACK/Hex/hex.py
@@ -1,4 +1,4 @@
 g = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
-print(bytes.fromhex(g))
+#print(bytes.fromhex(g))
 flag = bytes.fromhex(g).decode('ascii')
 print(flag)
